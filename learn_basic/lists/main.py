2ecc2072e9b9238654914c42099a4500fae5bd27
mnguyen0226 <mnguyen0226@vt.edu>
2025-02-28 15:20:47 -0500

    Added list code

---
+++ learn_basic/lists/main.py
@@ -0,0 +1,22 @@
+# List == Array
+
+# Array initialization
+my_arr = [1, 2] # 3 # 4
+print(my_arr)
+
+# Add value
+my_arr.append(3)
+print(my_arr)
+
+# Delete - last value
+my_arr.pop()
+print(my_arr)
+
+# Delete value at position
+my_arr.append(4)
+my_arr.append(5)
+print(my_arr)
+my_arr.remove(2)
+print(my_arr)
+
+# Assignment: Give array of name, add a name, remove last name, remove name at position 2
