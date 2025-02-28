e45537fcb44c834aa1bdbd181f9f35762a444949
mnguyen0226 <mnguyen0226@vt.edu>
2025-02-27 22:05:25 -0500

    Added code for var & type

---
+++ learn_basic/variables_and_types/main.py
@@ -0,0 +1,14 @@
+my_int = 8
+print("My int is: ", my_int)
+
+my_float = 8.0
+print("My float is: ", my_float)
+print(f"My float is: {my_float}")
+
+a = 1
+b = 2
+print(f"A + B = {a + b}")
+
+c = 1.0
+d = 1
+print(f"C + D = {c + d}")
