4fce0a72e36b867fc15df859d5cfe51d32899a4a
mnguyen0226 <mnguyen0226@vt.edu>
2025-02-28 15:40:54 -0500

    Added basic_operation

---
+++ learn_basic/basic_operations/main.py
@@ -0,0 +1,30 @@
+# + *
+number = 1 + 2 * 3 / 4.0
+print(number)
+
+# mod
+remainder = 11 % 3
+print(remainder)
+
+# ^
+squared = 7 ** 2
+cubed = 2 ** 3
+print(squared)
+print(cubed)
+
+# Add string
+helloworld = "hello" + " " + "world"
+print(helloworld)
+
+# String multi
+lotsofhellos = "hello" * 10
+print(lotsofhellos)
+
+# List
+even_numbers = [2,4,6,8]
+odd_numbers = [1,3,5,7]
+all_numbers = odd_numbers + even_numbers
+print(all_numbers)
+
+# List multi
+print([1,2,3] * 3)
